669a44d13a89005bfea61d013c384788bad29c12
Juan <juanfernandoherrada13@gmail.com>
2020-01-21 16:39:42 +0100

    small changes

--- bddClassement.sql
+++ bddClassement.sql
@@ -1,7 +1,19 @@
 CREATE TABLE Classement(
-    idCollab INT
+    idCollab INT,
     idClassement INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     classement_number INT(40) NOT NULL,
+    classement_point INT(40) UNSIGNED,
     
     FOREIGN KEY (idCollab) REFERENCES `Collab`(idCollab) ON DELETE CASCADE ON UPDATE CASCADE
 )
+
+CREATE TABLE resultatMatch(
+    idCollab INT,
+    idResultatMatch INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    resultatMatch_vainqueur INT (40) UNSIGNED,
+    resultatMatch_perdant INT (40) UNSIGNED,
+    resultatMatch_nul INT (40) UNSIGNED,
+
+    FOREIGN KEY (idCollab) REFERENCES `Collab`(idCollab) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
