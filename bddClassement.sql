a24ca560385bdebeb044a5c17029916033776345
DogSobre <juanfernandoherrada13@gmail.com>
2020-01-22 16:07:07 +0100

    trying to link mysql and php

--- bddClassement.sql
+++ bddClassement.sql
@@ -1,3 +1,5 @@
+------------------------ Table Classement ------------------------
+
 CREATE TABLE Classement(
     idCollab INT,
     idClassement INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -5,6 +7,29 @@ CREATE TABLE Classement(
     
     FOREIGN KEY (idCollab) REFERENCES `Collab`(idCollab) ON DELETE CASCADE ON UPDATE CASCADE
 )
+
+---------
+
+INSERT INTO `Classement`(`classement_point`) VALUES (31);
+INSERT INTO `Classement`(`classement_point`) VALUES (54);
+INSERT INTO `Classement`(`classement_point`) VALUES (02);
+INSERT INTO `Classement`(`classement_point`) VALUES (83);
+INSERT INTO `Classement`(`classement_point`) VALUES (29);
+INSERT INTO `Classement`(`classement_point`) VALUES (44);
+
+---------
+
+ALTER TABLE Classement
+ADD Collab VARCHAR(50);
+
+UPDATE `Classement` SET `Collab` = 'Player1' WHERE `Classement`.`idClassement` = 1;
+UPDATE `Classement` SET `Collab` = 'Player2' WHERE `Classement`.`idClassement` = 2;
+UPDATE `Classement` SET `Collab` = 'Player3' WHERE `Classement`.`idClassement` = 3;
+UPDATE `Classement` SET `Collab` = 'Player4' WHERE `Classement`.`idClassement` = 4;
+UPDATE `Classement` SET `Collab` = 'Player5' WHERE `Classement`.`idClassement` = 5;
+UPDATE `Classement` SET `Collab` = 'Player6' WHERE `Classement`.`idClassement` = 6;
+
+------------------------  Table résultat du match ------------------------
 
 CREATE TABLE resultatMatch(
     idCollab INT,
@@ -15,10 +40,3 @@ CREATE TABLE resultatMatch(
 
     FOREIGN KEY (idCollab) REFERENCES `Collab`(idCollab) ON DELETE CASCADE ON UPDATE CASCADE
 )
-
-INSERT INTO `Classement`(`classement_point`) VALUES (31);
-INSERT INTO `Classement`(`classement_point`) VALUES (54);
-INSERT INTO `Classement`(`classement_point`) VALUES (02);
-INSERT INTO `Classement`(`classement_point`) VALUES (83);
-INSERT INTO `Classement`(`classement_point`) VALUES (29);
-INSERT INTO `Classement`(`classement_point`) VALUES (44);
--- classement.php
+++ classement.php
@@ -1,4 +1,14 @@
-<?
+<?php
+//Paramétrage de la connexion à la base de données
+$db=  mysqli_connect('localhost:8889','root','root');
+mysqli_init();
 
-$db = new PDO('localhost', 'root', 'root');
+$recup_player = mysqli_query($db,'SELECT DISTINCT `Collab`, `classement_point` FROM `Classement` ORDER BY `classement_point`');
 
+$compteur = 1;
+while ($best_collab = mysqli_fetch_array($recup_player)){
+    echo $compteur;
+    echo $best_collab['Collab'];
+    echo $best_collab['classement_point'];
+    $compteur ++;
+}
