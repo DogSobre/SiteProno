03cfb6cd0d22a7be76f2b902e0f50405f696be85
DogSobre <juanfernandoherrada13@gmail.com>
2020-01-28 14:29:54 +0100

    small update from classement.php

--- classement.php
+++ classement.php
@@ -1,16 +1,16 @@
 <?php
 
-//Paramétrage de la connexion à la base de données
+// Paramétrage de la connexion à la base de données
 $database = localhost;
 $user = root;
 $pwd = root;
 
 try {
     $myPdo = new PDO('mysql:host=$database;dbname=inventory;charset=UTF-8', '$user' ,'$pwd');
-    $myPdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $myPdo -> setAttribute(ATTR_ERRMODE, ERRMODE_EXCEPTION);
 
-    $sth = $myPdo -> prepare('SELECT Collab; classement_point FROM Classmement ORDER BY classement_point DESC');
-    $result = $sth -> fetchAll(PDO::FETCH_ASSOC);
+    $sth = $myPdo -> prepare('SELECT Collab, classement_point FROM Classmement ORDER BY classement_point DESC');
+    $result = $sth -> fetchAll(FETCH_ASSOC);
     print_r($result);
 
     // Capture des exception si exception lancée, et affichage des info :
@@ -19,19 +19,18 @@ try {
     die();
 }
 
-$result =  PDOStatement::fetchAsso([ int, $bestCollab = "PDO::FETCH_BOTH"]);
+$result = fetchAsso([ int, $bestCollab = "FETCH_BOTH"]);
 
-//affichage du tableau de classement
-$compteur = 1;
-while($result -> execute($sth)) {
-    echo $compteur;
+$results = $myPdo -> query('SELECT Collab, classement_point FROM Classement');
+// Affichage du tableau de classement
+$count = 1;
+while(($data = $results -> fetch()) && ($count <= 10)) {
+    echo $count;
     echo $bestCollab['Collab'];
     echo $bestCollab['classement_point'];
-    $compteur++;
+    $count++;
 }
-
-?>
-
+$results -> closeCursor();
 
 
 /*$best_collab = 0 ;
