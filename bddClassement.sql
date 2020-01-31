e23440725b5463f6d06d237d64595624f903c75b
DogSobre <juanfernandoherrada13@gmail.com>
2020-01-31 09:36:49 +0100

    include the script of the connection to the bdd

--- bddClassement.sql
+++ bddClassement.sql
@@ -3,7 +3,6 @@
 CREATE TABLE Classement(
     idCollab INT,
     idClassement INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    classement_number INT(40) NOT NULL,
     classement_point INT(40) UNSIGNED,
     
     FOREIGN KEY (idCollab) REFERENCES `Collab`(idCollab) ON DELETE CASCADE ON UPDATE CASCADE
