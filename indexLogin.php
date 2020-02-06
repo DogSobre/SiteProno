250ad4a61a32fb01031d6b2b122fdcaff6748a47
DogSobre <juanfernandoherrada13@gmail.com>
2020-02-06 13:04:39 +0100

    another new test on indexLogin.php

--- indexLogin.php
+++ indexLogin.php
@@ -1,6 +1,68 @@
-<?php include 'index.php';?>
-
 <?php
+
+session_start();
+
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true){
+    header("location : indexAccueil.php");
+    exit;
+}
+
+require_once "index.php";
+
+$userMail = $password = "";
+$userMailErr = $passwordErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == $_POST){
+    if (empty(trim($_POST["Collab_Mail"]))){
+        $userMailErr = "Please enter email";
+    }
+    else{
+        $userMail = trim($_POST["Collab_Mail"]);
+    }
+
+    if (empty($userMailErr) && empty($passwordErr)){
+        $sql = "SELECT * FROM Collab WHERE Collab_Mail = :userMail";
+
+        if ($stmt = $db -> prepare($sql)){
+            $stmt -> bindParam(":userMail", $paramUsermail, PDO::PARAM_STR);
+
+            $paramUsermail = trim($_POST["Collab_Mail"]);
+            if ($stmt -> execute()){
+                if ($stmt -> rowCount() == 1){
+                    if ($row = $stmt -> fetch()){
+                        $id = $row["idCollab"];
+                        $userMail = $row["Collab_Mail"];
+                        $hashedPassword = $row["Collab_Password"];
+                        if (password_verify($password, $hashedPassword)){
+
+                            session_start();
+
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["idCollab"] = $id;
+                            $_SESSION["Collab_Mail"] = $userMail;
+                            header("location = indexAccueil.php");
+
+                        }
+                        else{
+                            $passwordErr = "Unvalid Password";
+                        }
+                    }
+                }
+                else{
+                    $userMailErr = "Unvalid eMail";
+                }
+            }
+            else{
+                echo "Something went wrong. Please try again.";
+            }
+        }
+        unset($stmt);
+    }
+    unset($db);
+}
+
+
+/*
 
 $login = $_POST['Collab_Mail'];
 $password = $_POST['Collab_Password'];
@@ -31,6 +93,7 @@ if (isset($_POST['submit'])){
         echo 'Please complete all boxes';
     }
 }
+*/
 
 /*
 if (isset($_POST['Collab_Mail']) && isset($_POST['Collab_Password'])){
@@ -56,7 +119,7 @@ else{
 */
 
 /*
-if (!emp ty($_POST['Collab_Name']) && !empty($_POST['Collab_Password'])) {
+if (!empty($_POST['Collab_Name']) && !empty($_POST['Collab_Password'])) {
     $username = PDO::quote($_POST['Collab_Name']);
     $password = PDO::quote($_POST['Collab_Password']);
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
@@ -114,7 +177,7 @@ if (!emp ty($_POST['Collab_Name']) && !empty($_POST['Collab_Password'])) {
                         </tr>
                         <tr>
                             <td>
-                                <input type="email" name="userMail" placeholder="collaborateur@carrefour.com" required >
+                                <input type="email" name="userMail" placeholder="collaborateur@carrefour.com" value="<?php echo $userMail; ?>" required >
                             </td>
                         </tr>
                         <tr>
